60cbe9177e30427c754845b3df9e17f54179c9a2
Abhishek <shek54112@gmail.com>
2026-01-28 21:34:07 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,126 @@
+# print("hello world")
+
+# name = "Abhishek Sehjal"
+# age = 22
+# city = "Nangal dam"
+# goal = "AI master"
+
+# print(name)
+# print(age)
+# print(city)
+# print(goal)
+
+# num1 = int(input('Enter your first number : '))
+# num2 = int(input('Enter your second number : '))
+
+# print("Sum is : " , num1 + num2)
+
+# length = float(input("Enter the length : "))
+# height = float(input("Enter the height : "))
+
+# print("Area of rectangle is : " , length * height)
+
+# celsius = float(input('Enter temperature in celsius : '))
+
+# fahrenheit = (9/5 * celsius) + 32
+# print(fahrenheit , "fahrenheit")
+
+# marks = [10,25,30]
+# avg = sum(marks) / len(marks)
+# print("Average of three numbers are : " , avg)
+
+# obtained = int(input("Enter your obtained marks : "))
+# total = int(input("Enter your total marks : "))
+# persentage = (obtained / total) * 100
+# print(persentage,"%")
+
+# num = int(input("Enter the number : "))
+
+# if num > 0:
+#     print("Positive number")
+# else:
+#     print("Negative number")
+
+# marks = int(input("Enter your marks : "))
+
+# if marks >= 33:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# age = int(input("Enter your age : "))
+
+# if age >= 18:
+#     print("You are adult")
+# else:
+#     print("You are child")
+
+# num1 = int(input("Enter 1st number : "))
+# num2 = int(input("Enter 2nd number : "))
+
+# if num1 > num2:
+#     print("Your first number is greater then second number")
+# else:
+#     print("Your second number is greater then first number") 
+
+# number = int(input("Enter your number : "))
+
+# if number > 10:
+#     print("Big")
+# else:
+#     print("small")
+
+# temperature = int(input("Enter temperature : "))
+
+# if temperature < 37:
+#     print("Normal")
+# else:
+#     print("Fever")
+
+
+
+
+# marks = int(input("Enter your marks : "))
+
+# if marks >= 80:
+#     print("Grade A")
+# elif marks >= 50 and marks <= 80:
+#     print("Grade B")
+# elif marks >= 30 and marks <= 50:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+
+
+
+# age = int(input("Enter your age : "))
+# nationality = input("Enter your nationality : ")
+
+# if age >= 18 and nationality == "indian":
+#     print("Your are elegible for vote")
+# else:
+#     print("Your are not elegible for vote")
+
+
+
+# username = "Abhishek"
+# password = 123456
+
+# usernameInput = input("Enter username : ")
+# passwordInput = int(input("Enter password : "))
+
+# if (usernameInput == username) and (passwordInput == password):
+#     print("Logged in successfully")
+# else:
+#     print("Login failed")
+
+
+temperature = int(input("Enter temperature : "))
+
+if temperature < 20:
+    print("Hot")
+elif temperature < 20 and temperature > 30:
+    print("Normal")
+else:
+    print("Cold")
