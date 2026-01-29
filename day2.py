8fb51f0250506a05689e40a1f7af1c8d3fab2e36
Abhishek <shek54112@gmail.com>
2026-01-30 00:49:31 +0530

    day 2 done

---
+++ day2.py
@@ -0,0 +1,130 @@
+# for i in range(5):
+#     print("Hello world")
+
+# for i in range(1,6):
+#     print(i)
+
+# i = 1
+
+# while i <= 6:
+#     print(i)
+#     i += 1
+
+# for i in range(1,11):
+#        print(i)
+
+# i = 1
+
+# while i <= 10:
+#        print(i)
+#        i += 1
+
+# for i in range(5, 0, -1):
+#     print(i)
+
+# i = 20
+# while i >= 10:
+#        print(i)
+#        i -= 1
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+# for i in range(1, 10):
+#     if i == 7:
+#         continue
+#     print(i)
+
+
+# for i in range(1, 10):
+#     if i == 5:
+#         break
+#     print(i)
+
+# for i in range(1, 5):
+#     for j in range(1, 3):
+#         print("day", i, "task", j)
+
+
+# for i in range(1, 5):
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
+
+
+# for i in range(5, 0, -1):
+#        for j in range(i):
+#               print("*", end="")
+#        print()
+
+
+# numbers = [10, 20, 30, 40]
+
+# for number in numbers:
+#        print(number)
+
+
+# numbers = [10, 20, 30, 40]
+
+# total = sum(numbers)
+# avg = total / len(numbers)
+
+# print("Total : ", total)
+# print("Average : ", avg)
+
+
+# numbers = []
+
+# for i in range(5):
+#     n = int(input("Enter number : "))
+#     numbers.append(n)
+# if len(numbers) == 5:
+#     print(numbers)
+#     print("Maximum : ", max(numbers))
+#     print("Minimum : ", min(numbers))
+#     print("Average : ", sum(numbers) / len(numbers))
+
+
+# Ques 1
+
+# correct_number = 3
+
+# for i in range(5):
+#        guess = int(input("Guess the correct number : "))
+#        if guess == correct_number:
+#               print("Found")
+#               break
+#        else:
+#               print("Not found")
+
+
+# Ques 2
+
+# nums = [1, 2, 2, 3, 4, 4, 5, 5, 6, 7, 6, 6]
+
+# unique = []
+
+# for i in nums:
+#        if i not in unique:
+#               unique.append(i)
+
+# print(unique)
+
+
+# Ques 3
+
+# odd_numbers = []
+
+# for i in range(5):
+#     number = int(input("Enter any number : "))
+#     if number % 2 != 0:
+#         odd_numbers.append(number)
+
+# print(odd_numbers)
