0d09af1670618e6d5ec18dec88400016cd178a81
Abhishek <shek54112@gmail.com>
2026-02-03 23:19:01 +0530

    day 4 completed

---
+++ day4.py
@@ -0,0 +1,52 @@
+# Qus 1
+
+# person = {
+#     "name" : "Abhishek",
+#     "age" : 21,
+#     "city" : "Nangal Dam"
+# }
+
+# for key in person:
+#     print(person[key])
+
+
+# Qus 2
+
+# def check_age(person):
+#     if person["age"] >= 18:
+#         return "Adult"
+#     else:
+#         return "Minor"
+
+# person = {
+#     "name" : "Abhishek",
+#     "age" : 18,
+#     "city" : "Nangal Dam"
+# }
+
+# result = check_age(person)
+# print(result)
+
+
+# Qus 3
+
+# students = [
+#     {"name": "A", "marks": 40},
+#     {"name": "B", "marks": 25},
+#     {"name": "C", "marks": 70}
+# ]
+
+# def count_pass(students):
+
+#     count = 0
+
+#     for student in students:
+#         if student["marks"] >= 33:
+#             count += 1
+
+#     return count
+
+
+# result = count_pass(students)
+
+# print(result)
